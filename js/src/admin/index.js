87e4cfb0137e207c01dbe73c03a9b2af0cca1262
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-09 19:14:57 -0400

    Update frontend for mithril 2

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -13,40 +13,40 @@ const getOptions = () =>
 
 app.initializers.add('fof/github-sponsors', () => {
     app.extensionSettings['fof-github-sponsors'] = () =>
-        app.modal.show(
-            new SettingsModal({
-                title: app.translator.trans('fof-github-sponsors.admin.settings.title'),
-                size: 'medium',
-                items: [
-                    <p>
-                        {app.translator.trans('fof-github-sponsors.admin.settings.desc', {
-                            a: <a href="https://github.com/settings/tokens" target="_blank" />,
-                        })}
-                    </p>,
-                    <StringItem key="fof-github-sponsors.api_token" required type="password">
-                        {app.translator.trans('fof-github-sponsors.admin.settings.api_token_label')}
-                    </StringItem>,
-                    [
-                        <label>{app.translator.trans('fof-github-sponsors.admin.settings.account_type_label')}</label>,
-                        <SelectItem key="fof-github-sponsors.account_type" options={getOptions()} required />,
-                    ],
-                    <StringItem key="fof-github-sponsors.login" required>
-                        {app.translator.trans('fof-github-sponsors.admin.settings.login_label')}
-                    </StringItem>,
-                    <div className="Form-group">
-                        <label>{app.translator.trans('fof-github-sponsors.admin.settings.group_label')}</label>
-
-                        {SelectItem.component({
-                            options: app.store.all('groups').reduce((o, g) => {
-                                o[g.id()] = g.nameSingular();
-
-                                return o;
-                            }, {}),
-                            key: 'fof-github-sponsors.group_id',
-                            required: true,
-                        })}
-                    </div>,
+        app.modal.show(SettingsModal, {
+            title: app.translator.trans('fof-github-sponsors.admin.settings.title'),
+            size: 'medium',
+            items: s => [
+                <p>
+                    {app.translator.trans('fof-github-sponsors.admin.settings.desc', {
+                        a: <a route="https://github.com/settings/tokens" target="_blank" />,
+                    })}
+                </p>,
+                <StringItem setting={s} name="fof-github-sponsors.api_token" required type="password">
+                    {app.translator.trans('fof-github-sponsors.admin.settings.api_token_label')}
+                </StringItem>,
+                [
+                    <label>{app.translator.trans('fof-github-sponsors.admin.settings.account_type_label')}</label>,
+                    <SelectItem setting={s} name="fof-github-sponsors.account_type" options={getOptions()} required />,
                 ],
-            })
+                <StringItem setting={s} name="fof-github-sponsors.login" required>
+                    {app.translator.trans('fof-github-sponsors.admin.settings.login_label')}
+                </StringItem>,
+                <div className="Form-group">
+                    <label>{app.translator.trans('fof-github-sponsors.admin.settings.group_label')}</label>
+
+                    {SelectItem.component({
+                        options: app.store.all('groups').reduce((o, g) => {
+                            o[g.id()] = g.nameSingular();
+
+                            return o;
+                        }, {}),
+                        name: 'fof-github-sponsors.group_id',
+                        setting: s,
+                        required: true,
+                    })}
+                </div>,
+            ],
+        }
         );
 });
