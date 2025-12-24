04b5c76ae5e2d568c366f7a20b4738254493736c
IanM <ian@morland.me>
2025-12-24 11:15:45 +0000

    chore: enable schedule

--- extend.php
+++ extend.php
@@ -24,12 +24,12 @@ return [
     new Extend\Locales(__DIR__.'/resources/locale'),
 
     (new Extend\Console())
-        ->command(UpdateCommand::class),
-    // ->schedule(UpdateCommand::class, function (Event $event) {
-    //     $paths = resolve(Paths::class);
+        ->command(UpdateCommand::class)
+    ->schedule(UpdateCommand::class, function (Event $event) {
+        $paths = resolve(Paths::class);
 
-    //     $event->hourly()
-    //         ->withoutOverlapping()
-    //         ->appendOutputTo($paths->storage.'/logs/fof-github-sponsors.log');
-    // }),
+        $event->hourly()
+            ->withoutOverlapping()
+            ->appendOutputTo($paths->storage.'/logs/fof-github-sponsors.log');
+    }),
 ];
